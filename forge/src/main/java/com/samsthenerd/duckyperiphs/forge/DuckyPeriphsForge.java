6a34a582d020b6412bb8950c60730884da1a0ce3
SamsTheNerd <sam@samsthenerd.com>
2023-05-14 00:31:35 -0400

    v1.2.0 update

--- forge/src/main/java/com/samsthenerd/duckyperiphs/forge/DuckyPeriphsForge.java
+++ forge/src/main/java/com/samsthenerd/duckyperiphs/forge/DuckyPeriphsForge.java
@@ -1,19 +1,14 @@
 package com.samsthenerd.duckyperiphs.forge;
 
-import java.util.List;
-
 import com.samsthenerd.duckyperiphs.DuckyPeriphs;
 import com.samsthenerd.duckyperiphs.DuckyPeriphsClientInit;
 import com.samsthenerd.duckyperiphs.forge.hexcasting.DuckyCastingCaps;
 
 import dan200.computercraft.api.ForgeComputerCraftAPI;
-import dev.architectury.event.events.common.LootEvent;
 import dev.architectury.platform.Platform;
 import dev.architectury.platform.forge.EventBuses;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.loot.LootPool;
-import net.minecraft.loot.LootTable;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -48,16 +43,16 @@ public class DuckyPeriphsForge {
 
     // forge is a miserable pile of mess, idk why it's different in such weird ways
     public static void addLootPools(){
-        LootEvent.MODIFY_LOOT_TABLE.register((lootTables, id, context, builtin) -> {
-			if (DuckyPeriphs.shouldAddKeyboards(id, builtin)) {
-				LootTable kbLootTable = lootTables.getTable(DuckyPeriphs.keyboardLootTable);
-                List<LootPool> pools = kbLootTable.f_79109_;
-                DuckyPeriphs.LOGGER.info("found " + pools.size() + " pools");
-                for (LootPool pool : pools) {
-                    DuckyPeriphs.LOGGER.info("Adding loot pool to " + id.toString() + ": " + pool.getName());
-                    context.addPool(pool);
-                }
-			}
-		});
+        // LootEvent.MODIFY_LOOT_TABLE.register((lootTables, id, context, builtin) -> {
+		// 	if (DuckyPeriphs.shouldAddKeyboards(id, builtin)) {
+		// 		LootTable kbLootTable = lootTables.getTable(DuckyPeriphs.keyboardLootTable);
+        //         List<LootPool> pools = kbLootTable.f_79109_;
+        //         DuckyPeriphs.LOGGER.info("found " + pools.size() + " pools");
+        //         for (LootPool pool : pools) {
+        //             DuckyPeriphs.LOGGER.info("Adding loot pool to " + id.toString() + ": " + pool.getName());
+        //             context.addPool(pool);
+        //         }
+		// 	}
+		// });
     }
 }
